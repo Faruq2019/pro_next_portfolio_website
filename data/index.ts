f4279c67adcd0ea13e27879c07608a0101a6e53e
Faruq2019 <umarfaruqqy@gmail.com>
2025-08-19 19:44:03 +0100

    add url link to my projects

--- data/index.ts
+++ data/index.ts
@@ -1,69 +1,162 @@
 export const projects = [
   {
-    title: 'Afronet Web App (Admin Panel)',
-    description: 'A full-stack match making application, built with Next.js and Node.js',
-    link: '#',
+    title: "Afronet Web App (Admin Panel)",
+    description:
+      "A full-stack match making application, built with Next.js and Node.js",
+    link: "https://adminpanel.afronetapp.com/",
   },
   {
-    title: 'Afrique Mobile App',
-    description: 'A financial platform designed to facilitate secure payments between individuals, such as a artisan and an employer. Built using Next.js and GraphQL for real-time interactions and seamless transactions.',
-    link: '#',
+    title: "Afrique Mobile App",
+    description:
+      "A financial platform designed to facilitate secure payments between individuals, such as a artisan and an employer. Built using Next.js and GraphQL for real-time interactions and seamless transactions.",
+    link: "https://apps.apple.com/us/app/afriquepay/id6739009253",
   },
   {
-    title: 'RentalBuzz',
-    description: 'A loan disbursement and staff management system, improving financial transparency. Built using React, React Native and Node.js',
-    link: '#',
+    title: "RentalBuzz",
+    description:
+      "A loan disbursement and staff management system, improving financial transparency. Built using React, React Native and Node.js",
+    link: "#",
   },
   {
-    title: 'wiPharmacy Web Application',
-    description: "A pharmacy  management web application, built with Angular and Node.js ",
-    link: "#"
+    title: "wiPharmacy Web Application",
+    description:
+      "A pharmacy  management web application, built with Angular and Node.js ",
+    link: "https://pharmacymhbenin.wicarehealth.com/",
   },
   {
-    title: 'wiWallet Web Application',
-    description: 'A financial management web application engineered to manage the finances of hospital and Clinics using Wicare application, enabling seamless transaction tracking. Built using Angular.js, PHP, and Node.js',
-    link: "#"
+    title: "wiWallet Web Application",
+    description:
+      "A financial management web application engineered to manage the finances of hospital and Clinics using Wicare application, enabling seamless transaction tracking. Built using Angular.js, PHP, and Node.js",
+    link: "http://ewalletmhbenin.wicarehealth.com/pages/login.php",
   },
   {
-    title: 'wiCare Web Application',
-    description: 'An hospital hospital management application, built to enhance the operational efficiency across departments. Built using Angular and Node.js',
-    link: "#"
-  }
-]
+    title: "wiCare Web Application",
+    description:
+      "An hospital hospital management application, built to enhance the operational efficiency across departments. Built using Angular and Node.js",
+    link: "https://mhbenin.wicarehealth.com/",
+  },
+];
 
 export const skills = [
-  { title: 'Agile Project Management', description: 'Managing projects using agile methodologies', link: '' },
-  { title: 'AWS', description: 'Amazon Web Services for cloud computing', link: '' },
-  { title: 'Angular', description: 'Front-end web application framework', link: '' },
-  { title: 'Angular.js', description: 'JavaScript framework for building web applications', link: '' },
-  { title: 'Bitbucket', description: 'Version control repository hosting', link: '' },
-  { title: 'Bootstrap', description: 'Front-end framework for developing responsive websites', link: '' },
-  { title: 'Cloud Technologies', description: 'Technologies for cloud computing services', link: '' },
-  { title: 'CSS', description: 'Style sheet language for web pages', link: '' },
-  { title: 'Debugging and Troubleshooting', description: 'Identifying and fixing bugs in software', link: '' },
-  { title: 'Digital Ocean', description: 'Cloud infrastructure provider', link: '' },
-  { title: 'Git', description: 'Version control system', link: '' },
-  { title: 'GitHub', description: 'Web-based version control hosting service', link: '' },
-  { title: 'HTML', description: 'Markup language for creating web pages', link: '' },
-  { title: 'Ionic', description: 'Framework for building mobile applications', link: '' },
-  { title: 'JavaScript', description: 'Programming language for web development', link: '' },
-  { title: 'jQuery', description: 'JavaScript library for DOM manipulation', link: '' },
-  { title: 'Linux', description: 'Open-source operating system', link: '' },
-  { title: 'MDB', description: 'Material Design for Bootstrap', link: '' },
-  { title: 'MongoDB', description: 'NoSQL database', link: '' },
-  { title: 'MySQL', description: 'Relational database management system', link: '' },
-  { title: 'Next.js', description: 'React framework for production', link: '' },
-  { title: 'Node.js', description: 'Server-side JavaScript', link: '' },
-  { title: 'Object-Oriented Programming (OOP)', description: 'Programming paradigm based on objects', link: '' },
-  { title: 'PHP', description: 'Server-side scripting language', link: '' },
-  { title: 'PostgreSQL', description: 'Advanced relational database management system', link: '' },
-  { title: 'React', description: 'Building interactive UIs', link: '' },
-  { title: 'React Native', description: 'Framework for building native mobile apps using React', link: '' },
-  { title: 'RESTful APIs', description: 'Architectural style for designing networked applications', link: '' },
-  { title: 'SQL', description: 'Structured query language for managing databases', link: '' },
-  { title: 'Tailwind CSS', description: 'Utility-first CSS framework for styling', link: '' },
-  { title: 'Version Control', description: 'System for managing changes to code', link: '' },
-  
+  {
+    title: "Agile Project Management",
+    description: "Managing projects using agile methodologies",
+    link: "",
+  },
+  {
+    title: "AWS",
+    description: "Amazon Web Services for cloud computing",
+    link: "",
+  },
+  {
+    title: "Angular",
+    description: "Front-end web application framework",
+    link: "",
+  },
+  {
+    title: "Angular.js",
+    description: "JavaScript framework for building web applications",
+    link: "",
+  },
+  {
+    title: "Bitbucket",
+    description: "Version control repository hosting",
+    link: "",
+  },
+  {
+    title: "Bootstrap",
+    description: "Front-end framework for developing responsive websites",
+    link: "",
+  },
+  {
+    title: "Cloud Technologies",
+    description: "Technologies for cloud computing services",
+    link: "",
+  },
+  { title: "CSS", description: "Style sheet language for web pages", link: "" },
+  {
+    title: "Debugging and Troubleshooting",
+    description: "Identifying and fixing bugs in software",
+    link: "",
+  },
+  {
+    title: "Digital Ocean",
+    description: "Cloud infrastructure provider",
+    link: "",
+  },
+  { title: "Git", description: "Version control system", link: "" },
+  {
+    title: "GitHub",
+    description: "Web-based version control hosting service",
+    link: "",
+  },
+  {
+    title: "HTML",
+    description: "Markup language for creating web pages",
+    link: "",
+  },
+  {
+    title: "Ionic",
+    description: "Framework for building mobile applications",
+    link: "",
+  },
+  {
+    title: "JavaScript",
+    description: "Programming language for web development",
+    link: "",
+  },
+  {
+    title: "jQuery",
+    description: "JavaScript library for DOM manipulation",
+    link: "",
+  },
+  { title: "Linux", description: "Open-source operating system", link: "" },
+  { title: "MDB", description: "Material Design for Bootstrap", link: "" },
+  { title: "MongoDB", description: "NoSQL database", link: "" },
+  {
+    title: "MySQL",
+    description: "Relational database management system",
+    link: "",
+  },
+  { title: "Next.js", description: "React framework for production", link: "" },
+  { title: "Node.js", description: "Server-side JavaScript", link: "" },
+  {
+    title: "Object-Oriented Programming (OOP)",
+    description: "Programming paradigm based on objects",
+    link: "",
+  },
+  { title: "PHP", description: "Server-side scripting language", link: "" },
+  {
+    title: "PostgreSQL",
+    description: "Advanced relational database management system",
+    link: "",
+  },
+  { title: "React", description: "Building interactive UIs", link: "" },
+  {
+    title: "React Native",
+    description: "Framework for building native mobile apps using React",
+    link: "",
+  },
+  {
+    title: "RESTful APIs",
+    description: "Architectural style for designing networked applications",
+    link: "",
+  },
+  {
+    title: "SQL",
+    description: "Structured query language for managing databases",
+    link: "",
+  },
+  {
+    title: "Tailwind CSS",
+    description: "Utility-first CSS framework for styling",
+    link: "",
+  },
+  {
+    title: "Version Control",
+    description: "System for managing changes to code",
+    link: "",
+  },
 ];
 
 export const message = `Highly skilled Full Stack Developer with 3 years of experience in designing and building scalable,
@@ -72,7 +165,7 @@ languages, frameworks, and tools, including back-end and front-end technologies,
 cloud infrastructure, and DevOps. Adept at leading cross-functional teams to deliver innovative and
 impactful software solutions. Demonstrated ability to optimize system performance, troubleshoot complex
 issues, and ensure the highest quality in code development. Committed to continuous learning and
-improvement, with a passion for enhancing user experiences and driving project success.`
+improvement, with a passion for enhancing user experiences and driving project success.`;
 
 export const navItems = [
   { name: "Home", link: "#home" },
